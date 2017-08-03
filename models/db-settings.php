2805f60b8a4566b763b964ff35b8ce9920e41513
Becky McCrea <dewby77@hotmail.com>
2017-08-03 07:06:03 -0500

    adding missing file

---
+++ models/db-settings.php
@@ -0,0 +1,78 @@
+<?php
+/*
+
+UserFrosting Version: 0.2.1 (beta)
+By Alex Weissman
+Copyright (c) 2014
+
+Based on the UserCake user management system, v2.0.2.
+Copyright (c) 2009-2012
+
+UserFrosting, like UserCake, is 100% free and open-source.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+//Database Information
+$db_host = "(local)\sqlexpress"; //Host address (most likely localhost)
+//$db_name = "croppro_new"; //Name of Database
+$db_name = "demo_new"; //Name of Database
+//$db_user = "root"; //Name of database user
+$db_user = ""; //Name of database user
+$db_pass = ""; //Password for database user
+$db_table_prefix = "demo_";
+
+// ----- EXAMPLE ------
+//$serverName = "(local)\sqlexpress";  
+
+/* Connect using Windows Authentication. */  
+//try  
+//{  
+//$conn = new PDO( "sqlsrv:server=$serverName ; Database=AdventureWorks", "", "");  
+//$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );  
+//}  
+//catch(Exception $e)  
+//{   
+//die( print_r( $e->getMessage() ) );   
+//}
+
+// All SQL queries use PDO now
+function pdoConnect(){
+	// Let this function throw a PDO exception if it cannot connect
+	global $db_host, $db_name, $db_user, $db_pass;
+	//$db = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_user, $db_pass);
+        $db = new PDO("sqlsrv:server=$db_host;Database=$db_name", $db_user, $db_pass);
+	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $db;
+}
+
+GLOBAL $errors;
+GLOBAL $successes;
+
+$errors = array();
+$successes = array();
+
+//Direct to install directory, if it exists
+if(is_dir("install/"))
+{
+	header("Location: install/");
+	die();
+
+}
